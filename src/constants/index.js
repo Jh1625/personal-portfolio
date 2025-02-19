2168e6a209ae63a97b68005e2acb3a4dc7e4d7d4
Jin Hui <jhui04291625@gmail.com>
2025-02-19 16:46:48 +0800

    experience done

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const EXPERIENCES = [
+    {
+        year: "7/2023 - 10/2023",
+        role: "Web Developer Intern",
+        company: "Ventopia Sdn Bjd",
+        description: `Collaborated with cross-functional teams to efficiently deliver client-requested tasks, ensuring high-quality outcomes. Contributed to the design and development of the company's in-house ERP system, implementing key features such as a calendar, task lists, login page, and database integrations to optimize internal operations. Additionally, created 3+ landing pages, resulting in increased client engagement and improved user experience.`,
+        technologies: ["Javascript", "PHP", "HTML5", "CSS", "BootStrap"], 
+    }
+]
